57bba368f85a4ee58903ab06f100ed0381836e63
Aniket2659 <aniket2659@gmail.com>
2024-07-08 13:17:38 +0530

    [Aniket] Add: python program to  read array

---
+++ functional_program/1_2d_array.py
@@ -0,0 +1,6 @@
+import numpy as np # import numpy for 2D array
+def Two_D():
+    two_D=np.array([[4,6],[7,9]]) # create 2D array
+    print(two_D)
+    print(two_D.ndim) # check the dimension
+Two_D()
